0f4752b8f5ea811a557eccf91685712374669403
ssoufii <salim_soufi@icloud.com>
2024-03-25 11:48:34 -0600

    blue screen now

--- AgoraApp/App.js
+++ AgoraApp/App.js
@@ -4,10 +4,11 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   //brings out terminal message whenever app executes
+  //one of the ways to debug
   console.log("App Executed")
   return (
     <View style={styles.container}>
-      <Text>Hello World</Text>
+      <Text>Hello World!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -15,8 +16,9 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
+    //flex:1 means container'll expand to fit whole screen 
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'dodgerblue',
     alignItems: 'center',
     justifyContent: 'center',
   },
