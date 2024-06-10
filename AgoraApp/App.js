431c8878c9bd6e36ed231898924662ef8c26e365
Hudeyfa Jimale <hudeyfa.jimale35@gmail.com>
2024-06-10 01:44:19 -0600

    test commit

--- AgoraApp/App.js
+++ AgoraApp/App.js
@@ -7,7 +7,7 @@ import LandingScreen from './src/screens/LandingScreen';
 import SignupScreen from './src/screens/SignUpScreen';
 import LoginScreen from './src/screens/LoginScreen';
 import HomeScreen from './src/screens/HomeScreen';
-
+import IndividualListing from './src/screens/IndividualListing';
 
 //Acts as a navigation controller for rest of screens
 
@@ -19,11 +19,12 @@ const Stack = createStackNavigator();
 function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator initialRouteName='IndividualListing'>
         <Stack.Screen name="Landing" component={LandingScreen} options={{ headerShown: false }}/>
         <Stack.Screen name="Signup" component={SignupScreen} options={{ headerShown: false }}/> 
         <Stack.Screen name="Login" component={LoginScreen} options={{ headerShown: false }}/> 
         <Stack.Screen name="Home" component={HomeScreen} options={{ headerShown: false }}/> 
+        <Stack.Screen name="IndividualListing" component={IndividualListing} options={{ headerShown: false }}/> 
       </Stack.Navigator>
     </NavigationContainer>
   );
